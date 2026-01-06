1ba9b9ca6d22c8833e080f60ca209530782fa208
Addison King <box@addisonking.com>
2026-01-06 12:31:55 -0500

    feat: add shared types utility file

---
+++ src/lib/types.ts
@@ -0,0 +1,17 @@
+/**
+ * Common type utilities for svtui components
+ */
+
+/**
+ * Adds a bindable ref property to component props
+ */
+export type WithRef<T extends HTMLElement = HTMLElement> = {
+	ref?: T | null;
+};
+
+/**
+ * Props for components that can render as either button or anchor
+ */
+export type PolymorphicButtonProps<T = {}> = T & {
+	href?: string;
+};
